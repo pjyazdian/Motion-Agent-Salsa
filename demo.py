Fix: Inference and rotation issues fixed.

demo.py
@@ -222,7 +222,7 @@ def motionllm_evaluation_qualitative_pairs():
     # model.load_model('output_trained/Second trial/Xmotionllm_epoch25.pth')
 
     # Baseline
-    model.load_model('output_trained/follower_to_leader/Xmotionllm_epoch91.pth')
+    model.load_model('output_trained/follower_to_leader_v2/Xmotionllm_epoch13.pth')
 
     model.llm.eval()
     model.llm.cuda()
@@ -230,9 +230,9 @@ def motionllm_evaluation_qualitative_pairs():
     motion_tokens_to_generate = []
     the_other_motion_tokens = []
 
-    Style='professional'
-    Pair="Pair3"
-    s, e = 40, 50
+    Style= 'beginner' # 'professional'
+    Pair="Pair1"
+    s, e = 0, 10
     items = load_data_pair(style=Style, pair=Pair, start_sec=s, end_sec=e)
 
 
@@ -264,11 +264,13 @@ def motionllm_evaluation_qualitative_pairs():
 
 
         motion_tokens = model.generate_Payam(full_prompt, input_ids)
+        # motion_tokens = vq_tokens_L
         motion_tokens_to_generate.append(motion_tokens)
 
         the_other_motion_tokens.append(vq_tokens_F if current_batch_task=='follower_to_leader' else vq_tokens_F)
 
-
+    #Todo ------------High priority---------------------
+    # We need to feed the first frame to keep continuity and etc.
     motion_tokens = torch.cat(motion_tokens_to_generate)
     motion = model.net.forward_decoder(motion_tokens)
     motion = model.denormalize(motion.detach().cpu().numpy())
@@ -286,7 +288,7 @@ def motionllm_evaluation_qualitative_pairs():
         plot_3d_motion(os.path.join(sav_path,
                         f"motionllm_{level}_{iterate}_predicted.mp4"),
                         t2m_kinematic_chain, positions.squeeze().detach().cpu().numpy(),
-                        title=(f"Generated: {Style} {Pair}"),
+                        title=(f"Generated {'leader' if current_batch_task=='follower_to_leader' else 'follower'}: {Style} {Pair}"),
                         fps=20, radius=4)
 
         #
@@ -323,7 +325,7 @@ def motionllm_evaluation_qualitative_pairs():
             plot_3d_motion(os.path.join(sav_path,
                                         f"motionllm_{level}_{iterate}_input.mp4"),
                            t2m_kinematic_chain, positions.squeeze().detach().cpu().numpy(),
-                           title=(f"Input: {Style} {Pair}"),
+                           title=(f"Input {'follower' if current_batch_task=='follower_to_leader' else 'leader'}: {Style} {Pair}"),
                            fps=20, radius=4)
 
             # vertices, faces = render_HQ_Salsa(positions.squeeze().detach().cpu().numpy(),

models/mllm.py
@@ -24,7 +24,7 @@ class MotionLLM(nn.Module):
         self.lora_config_t2m = LoraConfig(
             r=self.args.lora_r_t2m,
             lora_alpha=self.args.lora_alpha_t2m,
-            target_modules=['embed_tokens', 'lm_head',
+            target_modules=[ #'embed_tokens', 'lm_head',
                             'o_proj', 'q_proj', 'up_proj', 'v_proj', 'k_proj', 'down_proj', 'gate_proj'],
             lora_dropout=self.args.lora_dropout,
             bias="none",
@@ -107,8 +107,19 @@ class MotionLLM(nn.Module):
             self.llm = get_peft_model(self.llm, self.lora_config_t2m, adapter_name='t2m')
             # self.llm.add_adapter('m2t', self.lora_config_m2t)
 
+            pefti_llm = get_peft_model(self.llm, self.lora_config_t2m, adapter_name='t2m')
+            for name, param in pefti_llm.named_parameters():
+                if 'lora' in name:
+                    print(name)
 
 
+            # required_Grads:
+            # save the lm_head of the additional tokens
+            embeddings = self.llm.get_input_embeddings().weight[self.nb_text_tokens:]
+            lm_head = self.llm.lm_head.weight[self.nb_text_tokens:]
+
+            embeddings = pefti_llm.get_input_embeddings().weight[self.nb_text_tokens:]
+            lm_head = pefti_llm.lm_head.weight[self.nb_text_tokens:]
 
         self.llm.to(self.device)
         self.llm.eval()

train_motionllm_salsa.py
@@ -80,9 +80,9 @@ def main():
     args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     args.resume_ckpt = 'ckpt/motionllm.pth'
     args.use_wandb = True
-    args.task = "follower_to_leader"
+    args.task = 'follower_to_leader' # for pretraining #"follower_to_leader"
     args.wandb_project = "Salsa-LLM"
-    args.wandb_run_name = "pretrain_all" if not args.task else args.task
+    args.wandb_run_name = "pretrain_all" if not args.task else args.task + '_v2'
 
     args.save_dir = f'output_trained/{args.wandb_run_name}'
     os.makedirs(args.save_dir, exist_ok=True)
@@ -91,7 +91,8 @@ def main():
 
     model = MotionLLM(args)
     # Loading pretrained model for instruction fine-tuning
-    args.resume_ckpt = 'output_trained\pretrain_all/Xmotionllm_epoch8.pth'
+
+    args.resume_ckpt = None # 'output_trained\pretrain_all/Xmotionllm_epoch8.pth'
     if args.resume_ckpt and args.task:
         model.load_model(args.resume_ckpt)
 

utils/salsa_utils/libs/MotionScript/ms_utils.py
@@ -1622,13 +1622,14 @@ def get_pose_sequence_data_from_file_Salsa_Dance_preloaded(preloaded, normalizer
     # pose_seq_data = smplx_to_pos3d_SFU(loaded)
     #
     pose_seq_data = loaded['3d_keypoints']
-    j_seq = torch.tensor(pose_seq_data)
+    j_seq = torch.tensor(pose_seq_data).float()
     adjusted_trans = torch.from_numpy(loaded['trans'])
     # # Not required:
 
-    for frame_i in range(j_seq.shape[0]):
-        j_seq[frame_i] = transf(rotX, -90, j_seq[frame_i])
-        # adjusted_trans[frame_i] = transf(rotX, -90, adjusted_trans[frame_i].unsqueeze(0)).squeeze()
+    # We remove this since it is already applied at the LMDB step 1 generation.
+    # for frame_i in range(j_seq.shape[0]):
+    #     j_seq[frame_i] = transf(rotX, -90, j_seq[frame_i])
+    #     # adjusted_trans[frame_i] = transf(rotX, -90, adjusted_trans[frame_i].unsqueeze(0)).squeeze()
 
         # j_seq[frame_i] = transf(rotY, 180, j_seq[frame_i])
         # adjusted_trans[frame_i] = transf(rotY, 180, adjusted_trans[frame_i].unsqueeze(0)).squeeze()

utils/salsa_utils/salsa_utils.py
@@ -75,7 +75,7 @@ def salsa_smplx_to_pos3d(data):
 
     return keypoints3d
 
-
+import math
 from tqdm import tqdm
 from MotionScript.stmc_renderer.humor import HumorRenderer
 def sanity_check_vide(data):
@@ -116,6 +116,12 @@ def sanity_check_vide(data):
         progress_bar=tqdm,
     )
 
+rotX = lambda theta: torch.tensor([[1, 0, 0], [0, torch.cos(theta), -torch.sin(theta)], [0, torch.sin(theta), torch.cos(theta)]])
+rotY = lambda theta: torch.tensor([[torch.cos(theta), 0, torch.sin(theta)], [0, 1, 0], [-torch.sin(theta), 0, torch.cos(theta)]])
+rotZ = lambda theta: torch.tensor([[torch.cos(theta), -torch.sin(theta), 0], [torch.sin(theta), torch.cos(theta), 0], [0, 0, 1]])
+def transf(rotMat, theta_deg, values):
+    theta_rad = math.pi * torch.tensor(theta_deg).float() / 180.0
+    return rotMat(theta_rad).mm(values.t()).t()
 
 def Salsa_smplx_body_shape(data):
 
@@ -359,6 +365,8 @@ def read_all_salsa(base_path):
         db[i].sync()
         db[i].close()
 
+
+
 def read_all_salsa_pairs(base_path):
 
     out_path = os.path.join(base_path, 'lmdb_Salsa_pair')
@@ -439,6 +447,17 @@ def read_all_salsa_pairs(base_path):
                 keypoints3d_L = salsa_smplx_to_pos3d(dict_loaded_L)
                 keypoints3d_F = salsa_smplx_to_pos3d(dict_loaded_F)
 
+                # rotate -90 def around X-axisto be consistent with common datasets e.g., HumanML3D
+                keypoints3d_L = torch.tensor(keypoints3d_L).float()
+                keypoints3d_F = torch.tensor(keypoints3d_F).float()
+                for frame_i in range(keypoints3d_F.shape[0]):
+                    keypoints3d_L[frame_i] = transf(rotX, -90, keypoints3d_L[frame_i])
+                    keypoints3d_F[frame_i] = transf(rotX, -90, keypoints3d_F[frame_i])
+                keypoints3d_L = keypoints3d_L.numpy()
+                keypoints3d_F = keypoints3d_F.numpy()
+
+
+
                 rotmat_L = salsa_smplx_to_rotmat(loaded_leader)
                 rotmat_F = salsa_smplx_to_rotmat(loaded_follower)
 
@@ -463,7 +482,7 @@ def read_all_salsa_pairs(base_path):
                 HML3D_New_Joints_Vec_F = data_F
 
 
-
+                # ----- raw data
                 raw_euler_poses_L = loaded_leader['poses']
                 raw_euler_poses_F = loaded_follower['poses']
 
@@ -493,13 +512,18 @@ def read_all_salsa_pairs(base_path):
 
                 # np.save(pjoin(save_dir1, source_file), rec_ric_data.squeeze().numpy())
                 # np.save(pjoin(save_dir2, source_file), data)
-                # Todo: motion_representation.ipynb
-                # Done!
+                # Todo: motion_representation.ipynb --> Done!
+
 
                 # # Sanity Check:
-                # save_path = '1.gif'
+                #Todo: based on the sanity check, I noticed that we need to rotate the keypoints -90 degree to make
+                # the Salsa data consistent with common motion datasets such as HumanML3D
+                # save_path = '2.gif'
                 # # HM3D_F.plot_3d_motion(save_path, kinematic_chain, New_Joints[:20], title="None", fps=20, radius=4)
-                # ARGUS = HML3D_New_Joints[:400], save_path, 'title'
+                # goto_plot = torch.tensor(keypoints3d_L[:50]).float()
+                # for frame_i in range(goto_plot.shape[0]):
+                #     goto_plot[frame_i] = transf(rotX, -90, goto_plot[frame_i])
+                # ARGUS = goto_plot[:50].numpy(), save_path, 'title'
                 # HM3D_F.plot_3d_motion_Payam(ARGUS)
 
                 # for v_i, bvh_file in enumerate(bvh_files):